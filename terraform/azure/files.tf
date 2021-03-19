b5c23fed256cece5b2feb414aee4148828f18b8a
Kerim Satirli <k.satirli@gmail.com>
2021-03-19 13:19:34 +0100

    linting

--- terraform/azure/files.tf
+++ terraform/azure/files.tf
@@ -1,5 +1,5 @@
 locals {
-  terraform_data = templatefile("./templates/azure-terraform-template.auto.pkrvars.hcl", {
+  terraform_data = templatefile("templates/azure-terraform-template.auto.pkrvars.hcl", {
     location                          = azurerm_resource_group.packer.location
     managed_image_resource_group_name = azurerm_resource_group.packer.name
   })
