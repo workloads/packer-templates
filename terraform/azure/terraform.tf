c5f34ceca1a6e55829514bc61ffcee27dd62b43b
Kerim Satirli <k.satirli@gmail.com>
2021-03-27 12:19:07 +0100

    bumps Azure provider

--- terraform/azure/terraform.tf
+++ terraform/azure/terraform.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/azurerm/2.52.0
+    # see https://registry.terraform.io/providers/hashicorp/azurerm/2.53.0
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.52.0"
+      version = "2.53.0"
     }
 
     # see https://registry.terraform.io/providers/hashicorp/local/2.1.0
