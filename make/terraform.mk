08b21197f294f69a8a99d78e7880cb1c79835905
Kerim Satirli <k.satirli@gmail.com>
2021-03-28 14:17:46 +0200

    adds support for `auto-approve`

--- make/terraform.mk
+++ make/terraform.mk
@@ -1,16 +1,32 @@
+# configuration for Terraform-specific variables
+ifdef auto-approve
+terraform_auto_approve = -auto-approve
+else
+terraform_auto_approve =
+endif
+
+.PHONY: terraform-plan
+terraform-plan: # Plan prerequisite resources for a target with Terraform
+	@: $(if $(target),,$(call missing_target))
+	@terraform \
+		-chdir="./terraform/$(target)" \
+		plan \
+
 .PHONY: terraform-apply
 terraform-apply: # Create prerequisite resources for a target with Terraform
 	@: $(if $(target),,$(call missing_target))
 	@terraform \
 		-chdir="./terraform/$(target)" \
-		apply
+		apply \
+			$(terraform_auto_approve)
 
 .PHONY: terraform-destroy
 terraform-destroy: # Destroy prerequisite resources for a target with Terraform
 	@: $(if $(target),,$(call missing_target))
 	@terraform \
 		-chdir="./terraform/$(target)" \
-		destroy
+		destroy \
+			$(terraform_auto_approve)
 
 .PHONY: terraform-init
 terraform-init: # Initializes Terraform for a target
