504e02234c6553418443335e6af1fe442bbe432f
Kerim Satirli <k.satirli@gmail.com>
2021-03-31 13:32:53 +0200

    linting yaml

--- make/ansible.mk
+++ make/ansible.mk
@@ -3,7 +3,6 @@ ansible_playbooks = ansible/playbooks
 .PHONY: ansible-lint
 ansible-lint: # Lints Ansible playbook(s)
 	@yamllint \
-		--config-data "{ rules: {line-length: { max: 125 } } }" \
 		"$(ansible_playbooks)/$(wildcard *.yml)" \
 	&& \
 	ansible-lint \
