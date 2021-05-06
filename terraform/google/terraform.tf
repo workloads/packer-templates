513f845e22f7a06b1baeae119752279daf2fcae3
Kerim Satirli <k.satirli@gmail.com>
2021-05-06 11:26:52 +0200

    linting

--- terraform/google/terraform.tf
+++ terraform/google/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/google/3.64.0
+    # see https://registry.terraform.io/providers/hashicorp/google/3.66.1
     google = {
       source  = "hashicorp/google"
       version = "3.66.1"
