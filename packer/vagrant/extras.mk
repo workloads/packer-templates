d1fdb2d77626d171f2bea98838cba01865005028
Kerim Satirli <k.satirli@gmail.com>
2023-08-07 17:52:00 +0200

    linting

--- packer/vagrant/extras.mk
+++
@@ -1,9 +0,0 @@
-arg_processor_vagrant_cloud =
-
-# toggle for enabling uploads to Vagrant Cloud
-ifdef vagrant_cloud
-	arg_processor_vagrant_cloud = vagrant-cloud
-endif
-
-# additionally `-except` args to pass to `args_except`
-extra_except_args = $(arg_processor_vagrant_cloud)
--- packer/vagrant/shared.pkr.hcl
+++
@@ -1,1 +0,0 @@
-../shared.pkr.hcl
